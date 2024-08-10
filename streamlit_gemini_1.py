2d9b01bcffad626c4e332ec6a6afc9fbd5ae22ac
Rishabh Raj <68055586+rishabhr136@users.noreply.github.com>
2024-08-10 21:15:45 +0530

    Add files via upload

---
+++ streamlit_gemini_1.py
@@ -0,0 +1,46 @@
+import google.generativeai as genai
+import os
+os.environ['GOOGLE_API_KEY'] = "AIzaSyD4EKdGbVxYnB9Ua5GqqTxzQC-ETohzBIA"
+genai.configure(api_key = os.environ['GOOGLE_API_KEY'])
+
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+import streamlit as st
+import os
+import pathlib
+import textwrap
+
+import google.generativeai as genai
+
+from IPython.display import display
+from IPython.display import Markdown
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+
+## Function to load OpenAI model and get respones
+
+def get_gemini_response(question):
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(question)
+    return response.text
+
+st.set_page_config(page_title="Q&A Demo")
+
+st.header("Gemini Application")
+
+input=st.text_input("Input: ",key="input")
+
+
+submit=st.button("Ask the question")
+
+## If ask button is clicked
+
+if submit:
+    
+    response=get_gemini_response(input)
+    st.subheader("The Response is")
+    st.write(response)
---
+++ streamlit_gemini_2.py
@@ -0,0 +1,50 @@
+import google.generativeai as genai
+import os
+os.environ['GOOGLE_API_KEY'] = "AIzaSyD4EKdGbVxYnB9Ua5GqqTxzQC-ETohzBIA"
+genai.configure(api_key = os.environ['GOOGLE_API_KEY'])
+
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+import streamlit as st
+import os
+import pathlib
+import textwrap
+
+import google.generativeai as genai
+
+from IPython.display import display
+from IPython.display import Markdown
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+
+model = genai.GenerativeModel('gemini-pro')
+chat = model.start_chat(history=[])
+def get_gemini_response(question):
+    
+    response =chat.send_message(question,stream=True)
+    return response
+
+st.set_page_config(page_title="Q&A Demo")
+
+st.header("Gemini Application")
+
+input=st.text_input("Input: ",key="input")
+
+
+submit=st.button("Ask the question")
+
+## If ask button is clicked
+
+if submit:
+    
+    response=get_gemini_response(input)
+    st.subheader("The Response is")
+    for chunk in response:
+        print(st.write(chunk.text))
+        print("_"*80)
+    
+    st.write(chat.history)
